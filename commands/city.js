9474139a5741c9e7317e2deb968593455d781074
Mathieu Santiago <62888392+mathieusantiago@users.noreply.github.com>
2022-01-12 10:32:38 +0100

    clear data

--- commands/city.js
+++ commands/city.js
@@ -19,73 +19,120 @@ module.exports = {
       api = await result.json();
       api.resultats.map(async (data) => {
         //create cosntant for recieving data call of API
-        let money = "non renseigné";
+        let money = "non renseigner";
+        let offre = data;
+
+        //recieve data of title (developpeur junior)
+        const setTitle =
+          offre && offre.intitule
+            ? offre.intitule.substring(0, 256)
+            : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
+        //recieve data of url offre (https://www.pole-emploi.fr/offres/2718097)
+        const setURL =
+          offre && offre.origineOffre && offre.origineOffre.urlOrigine
+            ? offre.origineOffre.urlOrigine
+            : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
+        //recieve data of the offre (2021-11-10T16:52:53.000Z)
+        const dataCreate =
+          offre && offre.dateCreation
+            ? offre.dateCreation
+            : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
+        //recieve data of name of create offre (Micheline)
+        const contactName =
+          offre && offre.contact && offre.contact.nom
+            ? offre.contact.nom
+            : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
+        //recieve data of companie (Micheline ink)
+        const companieName =
+          offre && offre.entreprise && offre.entreprise.nom
+            ? offre.entreprise.nom
+            : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
+        //recieve data of city (dubai)
+        const city =
+          offre && offre.lieuTravail && offre.lieuTravail.libelle
+            ? offre.lieuTravail.libelle
+            : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
+        //recieve data of contrat (CDI CDD ...)
+        const typeContract =
+          offre && offre.typeContratLibelle
+            ? offre.typeContratLibelle
+            : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
+        //recieve data of contrat (35h/semaine)
+        const timeWork1 =
+          offre && offre.dureeTravailLibelleConverti
+            ? offre.dureeTravailLibelleConverti
+            : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
+        const timeWork2 =
+          offre && offre.dureeTravailLibelle
+            ? offre.dureeTravailLibelle
+            : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
         //if salary is undefined
-        if (data.salaire.libelle === undefined) {
+        if (offre.salaire.libelle === undefined) {
           money =
-            data && data.salaire && data.salaire.commentaire
-              ? data.salaire.commentaire
-              : defaultText;
+            offre && offre.salaire && offre.salaire.commentaire
+              ? offre.salaire.commentaire
+              : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
         } else {
           money =
-            data && data.salaire && data.salaire.libelle
-              ? data.salaire.libelle
-              : defaultText;
+            offre && offre.salaire && offre.salaire.libelle
+              ? offre.salaire.libelle
+              : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
         }
+        //recieve data of experience
+        const experience =
+          offre && offre.experienceLibelle
+            ? offre.experienceLibelle
+            : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
+        //recieve data of niveau d'etude
+        const learn1 =
+          offre &&
+          offre.formations &&
+          offre.formations[0] &&
+          offre.formations[0].domaineLibelle
+            ? offre.formations[0].domaineLibelle
+            : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
+        const learn2 =
+          offre &&
+          offre.formations &&
+          offre.formations[0] &&
+          offre.formations[0].niveauLibelle
+            ? offre.formations[0].niveauLibelle
+            : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
+        //recieve data of description
+        const description =
+          offre && offre.description
+            ? offre.description.substring(0, 1000)
+            : "Non renseigné plus de détaille en cliquant sur le titre de l'offre";
 
         //ues message builder to create message
         const embed = new MessageEmbed()
-          .setTitle(
-            `:computer: ${data.intitule.substring(0, 256) || defaultText}`
-          )
+
+          .setTitle(`:computer: ${setTitle}`)
           .setAuthor(
             "Pôle Emploi",
             "https://upload.wikimedia.org/wikipedia/fr/thumb/c/c0/Logo_P%C3%B4le_Emploi_2008.svg/2560px-Logo_P%C3%B4le_Emploi_2008.svg.png",
             "https://www.pole-emploi.fr/accueil/"
           )
-          .setURL(data.origineOffre.urlOrigine || defaultText)
-          .addField(
-            ":date: Date de création ",
-            data.dateCreation || defaultText
-          )
+          .setURL(setURL)
+          .addField(":date: Date de création ", dataCreate)
           .addField(
             ":bust_in_silhouette: Nom du Contact  ",
-            "**" + data.contact.nom || defaultText + "**"
+            "**" + contactName + "**"
           )
+          .addField(":factory: Nom de l'entreprise ", companieName)
+          .addField(":map: Lieux de travail ", city)
+          .addField(":bookmark_tabs: Type de contrat ", typeContract)
           .addField(
-            ":factory: Nom de l'entreprise ",
-            data.entreprise.nom || defaultText
-          )
-          .addField(
-            ":map: Lieu de travail ",
-            data.lieuTravail.libelle || defaultText
-          )
-          .addField(
-            ":bookmark_tabs: Type de contrat ",
-            data.typeContratLibelle || defaultText
-          )
-          .addField(
-            ":alarm_clock: durée de Travail ",
-            data.dureeTravailLibelleConverti ||
-              data.dureeTravailLibelle + " " + timeWork2
+            ":alarm_clock: duree de Travail ",
+            timeWork1 + " " + timeWork2
           )
           .addField("\u200b", ":money_with_wings: Salaire ", money, "\u200b")
-          .addField(
-            ":bar_chart: Expérience demandée ",
-            data.experienceLibelle || defaultText
-          )
+          .addField(":bar_chart: Experience demandée ", experience)
           .addField(
             ":man_student: Formation demandée ",
-            "Domaine : " + data.formations[0].domaineLibelle ||
-              defaultText +
-                " Niveau d'étude : " +
-                data.formations[0].niveauLibelle ||
-              defaultText
+            "Domaine : " + learn1 + " Niveau d'étude : " + learn2
           )
-          .addField(
-            ":information_source: Description ",
-            data.description.substring(0, 1000) || defaultText
-          )
+          .addField(":information_source: Description ", description)
           .setColor("#00b0f4")
           .setThumbnail(
             "https://upload.wikimedia.org/wikipedia/fr/thumb/c/c0/Logo_P%C3%B4le_Emploi_2008.svg/2560px-Logo_P%C3%B4le_Emploi_2008.svg.png"
@@ -94,23 +141,23 @@ module.exports = {
 
         //send message
         message.channel
-          .send({
-            content: "Offre annonce Pôle Emploi",
-            username: "Bot-Pôle-Emploi",
-            avatarURL: "https://i.imgur.com/AfFp7pu.png",
-            embeds: [embed],
-          })
+          // .send({
+          //   content: "Offre annonce Pôle Emploi",
+          //   username: "Bot-Pôle-Emploi",
+          //   avatarURL: "https://i.imgur.com/AfFp7pu.png",
+          //   embeds: [embed],
+          // })
           .then((message) => {
-            const embed = new MessageEmbed()
-              .setTitle(
-                `:sunny: CMD:[!city ${args}] message send by ${userName} channel: ${message.channel} !!!`
-              )
-              .setColor("#00b0f4");
-            log_channel.send({ embeds: [embed] });
+            // const embed = new MessageEmbed()
+            //   .setTitle(
+            //     `:sunny: CMD:[!city ${args}] message send by ${userName} channel: ${message.channel} !!!`
+            //   )
+            //   .setColor("#00b0f4");
+            // log_channel.send({ embeds: [embed] });
 
-            console.log(
-              `CMD:[!city] message send by ${userName} channel: ${message.channel} !!!`
-            );
+            // console.log(
+            //   `CMD:[!city] message send by ${userName} channel: ${message.channel} !!!`
+            // );
           })
           .catch((err) => {
             const embed = new MessageEmbed()
