67cbb67a5891fa180b18531942f91d0a5191e184
E.Bousk <e.bousk@gmail.com>
2022-01-13 19:04:35 +0100

    Randomize image, factorize code, cosmetic change and typos fix

--- commands/city.js
+++ commands/city.js
@@ -18,91 +18,91 @@ module.exports = {
     if (result.status === 206) {
       api = await result.json();
       api.resultats.map(async (data) => {
-        //create constant for recieving data call of API
-        let money = "non renseigner";
+        //create constant for receiving data call of API
+        let money = "Non renseigné";
         let offre = data;
 
-        //recieve data of title (developpeur junior)
+        //receive data of title (developpeur junior)
         const setTitle =
           offre && offre.intitule
             ? offre.intitule.substring(0, 256)
-            : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
-        //recieve data of url offre (https://www.pole-emploi.fr/offres/2718097)
+            : defaultText;
+        //receive data of url offre (https://www.pole-emploi.fr/offres/2718097)
         const setURL =
           offre && offre.origineOffre && offre.origineOffre.urlOrigine
             ? offre.origineOffre.urlOrigine
-            : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
-        //recieve data of the offre (2021-11-10T16:52:53.000Z)
+            : defaultText;
+        //receive data of the offre (2021-11-10T16:52:53.000Z)
         const dataCreate =
           offre && offre.dateCreation
             ? offre.dateCreation
-            : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
-        //recieve data of name of create offre (Micheline)
+            : defaultText;
+        //receive data of name of create offre (Micheline)
         const contactName =
           offre && offre.contact && offre.contact.nom
             ? offre.contact.nom
-            : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
-        //recieve data of companie (Micheline ink)
+            : defaultText;
+        //receive data of companie (Micheline ink)
         const companieName =
           offre && offre.entreprise && offre.entreprise.nom
             ? offre.entreprise.nom
-            : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
-        //recieve data of city (dubai)
+            : defaultText;
+        //receive data of city (dubai)
         const city =
           offre && offre.lieuTravail && offre.lieuTravail.libelle
             ? offre.lieuTravail.libelle
-            : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
-        //recieve data of contrat (CDI CDD ...)
+            : defaultText;
+        //receive data of contrat (CDI CDD ...)
         const typeContract =
           offre && offre.typeContratLibelle
             ? offre.typeContratLibelle
-            : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
-        //recieve data of contrat (35h/semaine)
+            : defaultText;
+        //receive data of contrat (35h/semaine)
         const timeWork1 =
           offre && offre.dureeTravailLibelleConverti
             ? offre.dureeTravailLibelleConverti
-            : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
+            : defaultText;
         const timeWork2 =
           offre && offre.dureeTravailLibelle
             ? offre.dureeTravailLibelle
-            : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
+            : defaultText;
         //if salary is undefined
         if (offre.salaire.libelle === undefined) {
           money =
             offre && offre.salaire && offre.salaire.commentaire
               ? offre.salaire.commentaire
-              : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
+              : defaultText;
         } else {
           money =
             offre && offre.salaire && offre.salaire.libelle
               ? offre.salaire.libelle
-              : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
+              : defaultText;
         }
-        //recieve data of experience
+        //receive data of experience
         const experience =
           offre && offre.experienceLibelle
             ? offre.experienceLibelle
-            : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
-        //recieve data of niveau d'etude
+            : defaultText;
+        //receive data of niveau d'etude
         const learn1 =
           offre &&
           offre.formations &&
           offre.formations[0] &&
           offre.formations[0].domaineLibelle
             ? offre.formations[0].domaineLibelle
-            : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
+            : defaultText;
         const learn2 =
           offre &&
           offre.formations &&
           offre.formations[0] &&
           offre.formations[0].niveauLibelle
             ? offre.formations[0].niveauLibelle
-            : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
-        //recieve data of description
+            : defaultText;
+        //receive data of description
         const description =
           offre && offre.description
             ? offre.description.substring(0, 1000)
-            : "Non renseigné. Plus de détails en cliquant sur le titre de l'offre";
+            : defaultText;
 
         //use message builder to create message
         const embed = new MessageEmbed()
@@ -114,30 +114,30 @@ module.exports = {
             "https://www.pole-emploi.fr/accueil/"
           )
           .setURL(setURL)
-          .addField(":date: Date de création ", dataCreate)
+          .addField(":date: Date de création :", dataCreate)
           .addField(
-            ":bust_in_silhouette: Nom du Contact  ",
+            ":bust_in_silhouette: Nom du Contact :",
             "**" + contactName + "**"
           )
-          .addField(":factory: Nom de l'entreprise ", companieName)
-          .addField(":map: Lieu de travail ", city)
-          .addField(":bookmark_tabs: Type de contrat ", typeContract)
+          .addField(":factory: Nom de l'entreprise :", companieName)
+          .addField(":map: Lieu de travail :", city)
+          .addField(":bookmark_tabs: Type de contrat :", typeContract)
           .addField(
-            ":alarm_clock: duree de Travail ",
-            timeWork1 + " " + timeWork2
+            ":alarm_clock: durée de Travail :",
+            timeWork1 + " - " + timeWork2
           )
-          .addField(":money_with_wings: Salaire ", money)
-          .addField(":bar_chart: Experience demandée ", experience)
+          .addField(":money_with_wings: Salaire :", money)
+          .addField(":bar_chart: Experience demandée :", experience)
           .addField(
             ":man_student: Formation demandée ",
-            "Domaine : " + learn1 + " Niveau d'étude : " + learn2
+            "Domaine : " + learn1 + "\nNiveau d'étude : " + learn2
           )
-          .addField(":information_source: Description ", description)
+          .addField(":information_source: Description :", description)
           .setColor("#00b0f4")
           .setThumbnail(
             "https://upload.wikimedia.org/wikipedia/fr/thumb/c/c0/Logo_P%C3%B4le_Emploi_2008.svg/2560px-Logo_P%C3%B4le_Emploi_2008.svg.png"
           )
-          .setImage("http://placeimg.com/800/400/tech");
+          .setImage("http://placeimg.com/800/400/tech?nocache=" + Math.floor(Math.random() *100));
 
         //send message
         message.channel
